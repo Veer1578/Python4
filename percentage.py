1853889c39ff1323c2e066410a9b8f1bac50c141
Veer1578 <sweetyatulshah@gmail.com>
2025-04-07 09:24:04 +0530

    percentage

---
+++ percentage.py
@@ -0,0 +1,12 @@
+math = int(input("Maths marks "))
+science = int(input("Science marks "))
+socialStudies = int(input("Social Studies marks "))
+marathi = int(input("Marathi marks "))
+
+sum  = math + science + socialStudies + marathi
+print(f"The boy's total marks are {sum}")
+
+totalSum = 400
+percentage = sum/totalSum * 100
+print(f"The boy's percentage is {percentage}%")
+
