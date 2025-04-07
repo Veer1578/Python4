b177352ce8e33e65ca444935f0a8dfb7d597a2c4
Veer1578 <sweetyatulshah@gmail.com>
2025-04-07 09:41:01 +0530

    task

---
+++ task.py
@@ -0,0 +1,9 @@
+#task 1
+equation = "[{(5**2) * 40} / 10] + 0.50 - 0.25"
+print(f"The answer of the equation whose solution come to 100.25 is {equation}")
+
+#task 2
+
+value = 3 + 1.5 + 4
+print(f"The type of 3 + 1.5 + 4 is {type(value)}")
+
