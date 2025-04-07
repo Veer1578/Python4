ada8496c161cad14faa84251fea8ad140ad1c4bd
Veer1578 <sweetyatulshah@gmail.com>
2025-04-07 09:09:56 +0530

    average

---
+++ average.py
@@ -0,0 +1,11 @@
+height1 = 98
+height2 = 91
+height3 = 41
+height4 = 95
+height5 = 11
+
+sum = height1 + height2 + height3 + height4 + height5
+print(f"The total height is {sum}")
+
+average = sum/5
+print(f"The average height is {average}")
